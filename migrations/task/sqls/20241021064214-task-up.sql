4b197879385627f4a60af38128d13a9fc587055c
benson850121 <benson850121@accudata.com.tw>
2024-11-29 10:28:58 +0800

    完成第三大題

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -163,9 +163,29 @@ VALUES(
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
+UPDATE
+  "COACH"
+SET
+  experience_years = 3
+WHERE
+	id = (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE name = '肌肉棒子'));
+
+UPDATE
+  "COACH"
+SET
+  experience_years = 5
+WHERE
+	id = (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE name = 'Q太郎'));
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+INSERT INTO
+  "SKILL"(name)
+VALUES('空中瑜伽');
 
+DELETE FROM
+  "SKILL"
+WHERE
+  name = '空中瑜伽';
 
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
