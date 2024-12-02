eecd0a434df6fbb5e611f57416904bd8160ef093
maplestartend <asercv14632@gmail.com>
2024-12-02 23:03:30 +0800

    完成6-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -378,9 +378,23 @@ LIMIT
   
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
-
+select
+	"CREDIT_PACKAGE"."name" as "組合包方案名稱",
+	count(*) as "銷售數量"
+from
+	"CREDIT_PURCHASE"
+inner join "CREDIT_PACKAGE" on
+	"CREDIT_PURCHASE".credit_package_id = "CREDIT_PACKAGE".id
+where
+	extract(month
+from
+	"CREDIT_PURCHASE".purchase_at) = 11
+group by
+	"CREDIT_PACKAGE"."name";
+  
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
+
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
